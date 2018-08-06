d12060632c55d68304390c01ce2465594e485576
PeaceDev <jonasd815@gmail.com>
2018-08-06 06:26:42 -0700

    second schedule

---
+++ simple/ProfileModule/View/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  simple
+//
+//  Created by Peace on 8/4/18.
+//  Copyright © 2018 Peace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
